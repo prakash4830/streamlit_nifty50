c0cceecda175019fa60cfa8fa2bd8226cc42a110
prakash4830 <61614799+prakash4830@users.noreply.github.com>
2024-10-12 19:40:58 +0530

    Update default home page main.py

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 # Define pages with their titles and icons
-def create_page(file_path, title, icon):
-    return st.Page(file_path, title=title, icon=icon)
+def create_page(file_path, title, icon, default):
+    return st.Page(file_path, title=title, icon=icon, default=default)
 
 def main():
     # Create pages
-    home_page = create_page("directory/eda.py", "Data Collection", icon=":material/data_check:")
-    page_1 = create_page("directory/regression.py", "Regression Analysis", icon=":material/search_insights:")  # Chart emoji
-    page_2 = create_page("directory/pattern.py", "Pattern Analysis", icon=":material/monitoring:")  # Bar chart emoji
-    about_page = create_page("directory/about.py", "About", icon=":material/account_circle:")  # Information emoji
+    home_page = create_page("directory/eda.py", "Data Collection", icon=":material/data_check:", default=True)
+    page_1 = create_page("directory/regression.py", "Regression Analysis", icon=":material/search_insights:", default=False) 
+    page_2 = create_page("directory/pattern.py", "Pattern Analysis", icon=":material/monitoring:", default=False) 
+    about_page = create_page("directory/about.py", "About", icon=":material/account_circle:", default=False)  
 
     # Set up navigation
     pages = {
